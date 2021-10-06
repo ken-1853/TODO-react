781cff72de2fb242ecc2662688d5f8857b9df3ca
Kentaro Wada <73265203+ken-1853@users.noreply.github.com>
2021-10-06 13:26:19 +0900

    add: タスクの個数制限

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,14 @@ export const App = () => {
 				todoText={todoText}
 				onChange={onChangeTodoText}
 				onClick={onClickAdd}
+				disabled={incompreteTodos.length >= 5}
 			/>
+
+			{incompreteTodos.length >= 5 && (
+				<p style={{ color: "red" }}>
+					登録できるTODOは5個までだよ〜〜！消化しよ〜
+				</p>
+			)}
 
 			<IncompleteTodos
 				todos={incompreteTodos}
